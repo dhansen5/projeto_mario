35e4f6bf9112b4529ae51f8542f455bef10460ae
Douglas Hansen <hansenndouglas@gmail.com>
2023-01-23 20:21:22 -0300

    Corrigido arquivo JS e finalizado projeto

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,21 @@
-const botaoTrailer = document.querySelector(".botao-trailer");      // Seleciona a class botao-trailer
-const botaoFecharModal = document.querySelector(".fechar-modal");   // Seleciona a class fechar-modal
-const video = document.getElementById("video");                     // Seleciona elemento pelo id "video"
-const modal = document.querySelector(".modal");                     // Seleciona a class modal
-const linkDoVideo = video.src;                                      // Seleciona a propriedade "src" do elemento com id "video"
+const botaoTrailer = document.querySelector(".botao-trailer"); // Seleciona a class botao-trailer
+const botaoFecharModal = document.querySelector(".fechar-modal"); // Seleciona a class fechar-modal
+const video = document.getElementById("video"); // Seleciona elemento pelo id "video"
+const modal = document.querySelector(".modal"); // Seleciona a class modal
+const linkDoVideo = video.src; // Seleciona a propriedade "src" do elemento com id "video"
 
-function alternarModal(){
-    modal.classList.toggle("aberto");                  // Adiciona ou remove "aberto" da clas "modal" Ex: Modal -> modal aberto / modal aberto -> modal
+function alternarModal() {
+  modal.classList.toggle("aberto"); // Adiciona ou remove "aberto" da clas "modal" Ex: Modal -> modal aberto / modal aberto -> modal
 }
 
-botaoTrailer.addEventListener("click", () => {          //  Quando o botaoTrailer receber um "click", executa:
-    alternarModal();                                    //  a função alternarModal();
-    video.setAttribute("src", linkDoVideo);             //  seta o valor do atributo "src" com o valor de "linkDoVideo"
+botaoTrailer.addEventListener("click", () => {
+  //  Quando o botaoTrailer receber um "click", executa:
+  alternarModal(); //  a função alternarModal();
+  video.setAttribute("src", linkDoVideo); //  seta o valor do atributo "src" com o valor de "linkDoVideo"
 });
 
-botaoFecharModal.addEventListener("click", () => {      // Quando o botaoFecharModal receber um "click", executa:
-    alternarModal();                                    // a função alternarModal();
-    video.setAttribute("src", "");                      // seta o valor do atributo "src" para "vazio"
+botaoFecharModal.addEventListener("click", () => {
+  // Quando o botaoFecharModal receber um "click", executa:
+  alternarModal(); // a função alternarModal();
+  video.setAttribute("src", ""); // seta o valor do atributo "src" para "vazio"
 });
