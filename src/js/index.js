95bef9f9a79269b583dc9f388f7331a7ddacdef0
Douglas Hansen <97894066+dhansen5@users.noreply.github.com>
2023-01-23 12:14:37 -0300

    Atualizado index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,19 @@
-// const botaoTrailer = document.querySelector(".botao-trailer");
-// const botaoFecharModal = document.querySelector(".fechar-modal");
-// const video = document.getElementById("video");
-// const modal = document.querySelector(".modal");
-// const linkDoVideo = video.src;
+const botaoTrailer = document.querySelector(".botao-trailer");      // Seleciona a class botao-trailer
+const botaoFecharModal = document.querySelector(".fechar-modal");   // Seleciona a class fechar-modal
+const video = document.getElementById("video");                     // Seleciona elemento pelo id "video"
+const modal = document.querySelector(".modal");                     // Seleciona a class modal
+const linkDoVideo = video.src;                                      // Seleciona a propriedade "src" do elemento com id "video"
 
-// function alternarModal(){
-//     modal.classList. toggle("aberto");
-// }
+function alternarModal(){
+    modal.classList.toggle("aberto");                  // Adiciona ou remove "aberto" da clas "modal" Ex: Modal -> modal aberto / modal aberto -> modal
+}
 
-// botaoTrailer.addEventListener("click", () => {
-//     alternarModal();
-//     video.setAttribute("src", linkDoVideo);
-// });
+botaoTrailer.addEventListener("click", () => {          //  Quando o botaoTrailer receber um "click", executa:
+    alternarModal();                                    //  a função alternarModal();
+    video.setAttribute("src", linkDoVideo);             //  seta o valor do atributo "src" com o valor de "linkDoVideo"
+});
 
-// botaoFecharModal.addEventListener("click", () => {
-//     alternarModal();
-//     video.setAttribute("src", "");
-// });
-
-alert('ooi');
+botaoFecharModal.addEventListener("click", () => {      // Quando o botaoFecharModal receber um "click", executa:
+    alternarModal();                                    // a função alternarModal();
+    video.setAttribute("src", "");                      // seta o valor do atributo "src" para "vazio"
+});
